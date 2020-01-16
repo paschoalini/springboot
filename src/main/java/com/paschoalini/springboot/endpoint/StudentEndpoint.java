ab3193859bc27479909b13e42f60ea93deef335c
Glauber Roberto Paschoalini <prglauber@gmail.com>
2020-01-15 22:32:35 -0300

    Conteudo da Aula 06

--- src/main/java/com/paschoalini/springboot/endpoint/StudentEndpoint.java
+++ src/main/java/com/paschoalini/springboot/endpoint/StudentEndpoint.java
@@ -2,9 +2,11 @@ package com.paschoalini.springboot.endpoint;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,18 @@ public class StudentEndpoint {
 	public ResponseEntity<?> save(@RequestBody Student student) {
 		Student.studentList.add(student);
 		return new ResponseEntity<>(student, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody Student student) {
+		Student.studentList.remove(student);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> update(@RequestBody Student student) {
+		Student.studentList.remove(student); // Remove pelo ID
+		Student.studentList.add(student); // Adiciona completo e alterado
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
