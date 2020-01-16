7d682a18aea58d678bb5d02b2d9e4fc57cae8b4f
Glauber Roberto Paschoalini <prglauber@gmail.com>
2020-01-15 21:56:24 -0300

    Conteudo da Aula 04

--- src/main/java/com/paschoalini/springboot/endpoint/StudentEndpoint.java
+++ src/main/java/com/paschoalini/springboot/endpoint/StudentEndpoint.java
@@ -2,22 +2,19 @@ package com.paschoalini.springboot.endpoint;
 
 import static java.util.Arrays.asList;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paschoalini.springboot.models.Student;
-import com.paschoalini.springboot.util.DateUtil;
 
 @RestController
 @RequestMapping("/student")
 public class StudentEndpoint {
-	@Autowired
-	private DateUtil dateUtil;
+	//@Autowired
+	//private DateUtil dateUtil;
 	
 	@RequestMapping(method = RequestMethod.GET, path="/list")
 	public List<Student> listAll() {
